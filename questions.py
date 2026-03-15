93fe50f0eb70bc8af4f27f5eca0e9cf4e7c7d34d
Joaquin Meza <mezajoaquin815@gmail.com>
2026-03-15 16:20:39 -0300

    Modificacion para reintentar el juego sin que se repitan palabras

--- questions.py
+++ questions.py
@@ -1,9 +1,10 @@
 import random
+
 categoria = {
-    "Conceptos" : ["python","programa"],
-    "Estructuras" : ["funcion","bucle"],
-    "Datos" : ["cadena","entero","lista"],
-    "Sintaxis" : ["variable"]
+    "Conceptos": ["python", "programa"],
+    "Estructuras": ["funcion", "bucle"],
+    "Datos": ["cadena", "entero", "lista"],
+    "Sintaxis": ["variable"],
 }
 menu = """ 
 //////////////////////////////////////
@@ -17,62 +18,97 @@ Seleccione una categoria
 --------------------------------------
 Ingrese su opcion (1-4):
 """
-#Menu de opciones de categoria, se elige un valor aleatorio 
-opcion = input(menu)
-match opcion:
-    case "1":
-        word = (categoria["Conceptos"][random.randint(0,1)])
-    case "2":
-        word = (categoria["Estructuras"][random.randint(0,1)])
-    case "3":
-        word = (categoria["Datos"][random.randint(0,2)])
-    case "4":
-        word = (categoria["Sintaxis"][0])
-    case _:            #Si se ingresa un caracter distinto se asigna la categoria conceptos
-        print('Se asigno la categoria Conceptos')
-        word = (categoria["Conceptos"][random.randint(0,1)])
-
-guessed = []
-attempts = 6
-score = 0
+reintento = True
 
 
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
+while reintento == True:
+    # Menu de opciones de categoria
+    opcion = input(menu)
+    match opcion:
+        case "1":
+            eleccion = categoria["Conceptos"]
+        case "2":
+            eleccion = categoria["Estructuras"]
+        case "3":
+            eleccion = categoria["Datos"]
+        case "4":
+            eleccion = categoria["Sintaxis"]
+
+        # Si se ingresa un caracter distinto se asigna la categoria conceptos
+        case _:
+            print("Se asigno la categoria Conceptos")
+            eleccion = categoria["Conceptos"]
+
+    cont = True
+    pos = 0
+
+    # Toma la cantidad de elementos y devuelve una lista con los mismos valores pero mezclada.
+    cant_elementos = len(eleccion)
+    mezcla = random.sample(eleccion, cant_elementos)
+    while cont and pos < cant_elementos:
+        word = mezcla[pos]
+        guessed = []
+        attempts = 6
+        score = 0
+
+        while attempts > 0:
+            # Mostrar progreso: letras adivinadas y guiones para las que faltan
+            progress = ""
+            for letter in word:
+                if letter in guessed:
+                    progress += letter + " "
+                else:
+                    progress += "_ "
+                    print(
+                        progress
+                    )  # Verificar si el jugador ya adivinó la palabra completa
+
+            if "_" not in progress:
+                score += 6
+                print("¡Ganaste!")
+                print(f"Tu puntaje es {score}")
+                pos += 1
+                opc = int(
+                    input("¿Quieres continuar? (0 = No continuar ---- 1 = Continuar)")
+                )
+                if opc != 1:
+                    cont = False
+                break
+
+            print(f"Intentos restantes: {attempts}")
+            print(f"Letras usadas: {', '.join(guessed)}")
+
+            letter = input("Ingresá una letra: ")
+            # Comprueba el rango y si el caracter ingresa es una letra
+            if len(letter) >= 2 or not letter.isalpha():
+                print("Entrada no valida")
+            else:
+                if letter in guessed:
+                    print("Ya usaste esa letra.")
+                elif letter in word:
+                    guessed.append(letter)
+                    print("¡Bien! Esa letra está en la palabra.")
+                else:
+                    guessed.append(letter)
+                    attempts -= 1
+                    score -= 1
+                    print("Esa letra no está en la palabra.")
+                    print()
         else:
-            progress += "_ "
-            print(progress)# Verificar si el jugador ya adivinó la palabra completa
-
-    if "_" not in progress:
-        score += 6
-        print("¡Ganaste!")
-        print(f"Tu puntaje es {score}")
-        break
-
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-
-    letter = input("Ingresá una letra: ")
-    # Comprueba el rango y si el caracter ingresa es una letra
-    if len(letter) >= 2 or not letter.isalpha():
-        print("Entrada no valida")
-    else:
-        if letter in guessed:
-            print("Ya usaste esa letra.")
-        elif letter in word:
-            guessed.append(letter)
-            print("¡Bien! Esa letra está en la palabra.")
-        else:
-            guessed.append(letter)
-            attempts -= 1
-            score -= 1
-            print("Esa letra no está en la palabra.")
-            print()
-else:
-    score = 0
-    print(f"¡Perdiste! La palabra era: {word}")
-    print(f"Tu puntaje es {score}")
+            score = 0
+            print(f"¡Perdiste! La palabra era: {word}")
+            print(f"Tu puntaje es {score}")
+            pos += 1
+            opc = int(
+                input("¿Quieres continuar? (0 = No continuar ---- 1 = Continuar)")
+            )
+            if opc != 1:
+                cont = False
+        if not cont or pos >= cant_elementos:
+            opc = int(
+                input(
+                    "¿Quieres continuar jugando con otra categoria? (0 = No continuar ---- 1 = Continuar)"
+                )
+            )
+            if opc != 1:
+                reintento = False
